e91ff7ab0186c1b5faafaa8e89678dead28d0a50
axvargas <axvargas@fiec.espol.edu.ec>
2020-05-06 01:24:03 -0500

    correccion de styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from 'react';
-import { Typography, Grid, Box } from '@material-ui/core';
+import { Typography, Grid, Box} from '@material-ui/core';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import Form from '../components/form';
 import AppointmentList from '../components/appointmentList'
-
+import style from './style';
 import {
 	createMuiTheme,
 	responsiveFontSizes,
@@ -16,25 +16,26 @@ theme = responsiveFontSizes(theme);
 
 const App = ({ createAppointment, globalAppointments, deleteAppointment }) => {
 
-
+	const classes = style();
 	return (
 		<Fragment>
 			<MuiThemeProvider theme={theme}>
 				<MuiPickersUtilsProvider utils={DateFnsUtils}>
 
+					
+						<Typography variant="h2"><Box fontWeight="fontWeightBold">Patient appointments</Box></Typography>
 
-					<Typography variant="h2"><Box fontWeight="fontWeightBold">Patient appointments</Box></Typography>
+						<Grid container spacing={3} className={classes.grid}>
+							<Grid item lg={6} xl={6} md={6} sm={12} xs={12} >
 
-					<Grid container spacing={3}>
-						<Grid item md={6} sm={12} xs={12}>
+								<Form createAppointment={createAppointment} />
 
-							<Form createAppointment={createAppointment} />
-
+							</Grid>
+							<Grid item lg={6} xl={6} md={6} sm={12} xs={12} >
+								<AppointmentList globalAppointments={globalAppointments} deleteAppointment={deleteAppointment} />
+							</Grid>
 						</Grid>
-						<Grid item md={6} sm={12} xs={12}>
-							<AppointmentList globalAppointments={globalAppointments} deleteAppointment={deleteAppointment} />
-						</Grid>
-					</Grid>
+					
 
 
 
--- src/app/style.js
+++ src/app/style.js
@@ -1,12 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: 2,
-      textAlign: 'center',
-      color: 'secondary',
+    
+    grid: {
+      width: '100%',
+      margin: '0px',
     },
     
   });
